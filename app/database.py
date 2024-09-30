35cec72dc840a526a45f3dbdfda05af58a9adea8
Michael Ricci <154825271+MichaelRicci@users.noreply.github.com>
2024-09-29 20:30:04 -0400

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,1 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class APTActivity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    description = db.Column(db.Text)
+    link = db.Column(db.String(200))
 
